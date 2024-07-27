dc7d3d039d673d63983394bc9e63eed240ca8aad
Durgesh Pawar <durgeshpawar077@gmail.com>
2024-07-27 18:55:24 +0530

    Changes for Test
    Ok

--- Test.java
+++ Test.java
@@ -7,7 +7,6 @@ public class Test {
 	public static void main(String[] args) {
 		
 		System.out.println("This is a Java Project");
-		System.out.println("It is Sample for Test");
 		System.out.println(new Date());
 		System.out.println("Second Commit");
 		System.out.println("Added one more file: "+new Date());
