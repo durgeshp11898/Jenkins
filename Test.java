35d5b977e644b80416f5523f37fdd60db287bd7a
Durgesh Pawar <durgeshpawar077@gmail.com>
2024-07-27 18:19:56 +0530

    Initial commit

---
+++ Test.java
@@ -0,0 +1,13 @@
+
+
+import java.util.Date;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is a Java Project");
+		System.out.println("It is Sample for Test");
+		System.out.println(new Date());
+	}
+}
