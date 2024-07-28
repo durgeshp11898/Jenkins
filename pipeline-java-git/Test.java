8fc966c176c7b565b0e2435e1ed91cb82f0fa3a2
Durgesh Pawar <durgeshpawar077@gmail.com>
2024-07-28 18:47:45 +0530

    ok for commit

---
+++ pipeline-java-git/Test.java
@@ -0,0 +1,14 @@
+
+
+import java.util.Date;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is a Java Project");
+		System.out.println(new Date());
+		System.out.println("Second Commit");
+		System.out.println("Added one more file: "+new Date());
+	}
+}
